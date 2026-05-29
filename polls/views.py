c3108186134827c213eddaa94430d1874d528241
阿瓦达啃麻瓜 <awadakenmagua@192.168.13.95>
2026-05-30 00:51:19 +0800

    Q9 demo failing lint gate

--- polls/views.py
+++ polls/views.py
@@ -221,3 +221,5 @@ def end_poll(request, poll_id):
         return render(request, 'polls/poll_result.html', {'poll': poll})
     else:
         return render(request, 'polls/poll_result.html', {'poll': poll})
+
+import json  # Q9 intentional unused import
